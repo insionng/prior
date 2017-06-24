77506e2185e08898655ed32d6e6e51a8c5efffc9
insion <insion@nilyes.com>
2017-06-24 20:10:56 +0800

    add test

--- priority_queue_test.go
+++ priority_queue_test.go
@@ -1,24 +1,31 @@
 package prior
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestPriorityQueue(t *testing.T) {
 	pq := NewPriorityQueue()
-	n1 := NewNode(1, 1, 2)
-	n2 := NewNode(2, 2, 3)
-	n3 := NewNode(3, 3, 4)
+	n1 := NewNode("bootstrap", func() {
+		fmt.Println("bootstrap")
+	}, 2)
+	n2 := NewNode("start", 2, 3)
+	n3 := NewNode(3, "value", 4)
 	pq.Push(n1)
 	pq.Push(n2)
 	pq.Push(n3)
 
 	v := pq.Pop()
-	if v.GetKey().(int) != 1 {
+	if v.GetKey().(string) != "bootstrap" {
 		t.Fatal()
+	} else {
+		if function, okay := v.GetValue().(func()); okay {
+			function()
+		}
 	}
 	v = pq.Pop()
-	if v.GetKey().(int) != 2 {
+	if v.GetKey().(string) != "start" {
 		t.Fatal()
 	}
 	v = pq.Pop()
@@ -38,12 +45,16 @@ func TestPriorityQueue(t *testing.T) {
 	pq.Remove(3)
 
 	v = pq.Pop()
-	if v.GetKey().(int) != 1 {
+	if v.GetKey().(string) != "bootstrap" {
 		t.Fatal()
 	}
 	v = pq.Pop()
 	if v.GetKey().(int) != 3 {
 		t.Fatal()
+	} else {
+		if v.GetValue().(string) != "value" {
+			t.Fatal()
+		}
 	}
 	v = pq.Pop()
 	if v != nil {
